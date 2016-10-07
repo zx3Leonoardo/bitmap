dc4664beee81a38ddd6a6aeb21282fbb44d19491
zx3Leonoardo <zx3.Leonardo@gmail.com>
2016-10-07 10:35:17 +0800

    Update bitmap.cc

--- src/bitmap.cc
+++ src/bitmap.cc
@@ -28,7 +28,24 @@ void ReadData(string data_path)
       ip_data.push_back(ip);
       cnt = 0;
     }
-
+  unsigned int i,j,k;
+	for(i=0;i<ip_data.size();i++)
+	{
+		for(j=0;j<8;j++)
+		{
+			bitmap_row.data[j*256+int(ip_data[i].data[j])-1] = 1;
+		}
+		bitmap.push_back(bitmap_row);
+		for(k=0;k<256*8;k++)
+			bitmap_row.data[k] = 0;
+	}
+	//transform
+	for(i=0;i<256*8;i++)
+	{
+		for(j=0;j<ip_data.size();j++)
+			final_bitmap[i].push_back(bitmap[j].data[i]);
+	}
+	file.close();
   sample_number = ip_data.size();
 }
 
