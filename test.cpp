1df067f657778ee1cf183ddb99fc5c5c9b9ae995
MasterXiong <harryfootball@163.com>
2016-10-08 12:27:09 +0800

    delete folders, deal with illegal query input

--- test.cpp
+++ test.cpp
@@ -14,13 +14,28 @@ int main() {
 
 	int query_index[8];
 	char option;
+        bool illegal_input;
 	while (1) {
 		cout << "please input the ip address:" << endl;
 		cout << "(missing address please type -1)" << endl;
 		for (int i = 0; i < 8; ++i) {
 			cin >> query_index[i];
 		}
+
+                illegal_input = 0;
+                for (int i = 0; i < 8; ++i) {
+                  if (query_index[i] > 255 || query_index[i] < -1) {
+                    illegal_input = 1;
+                    break;
+                  }
+                }
+                if (illegal_input) {
+                  cout << "illegal query input, please try again" << endl << endl;
+                  continue;
+                }
+
 		bitmap_test->ResponseQuery(query_index);
+
 		cout << "continue or not?  y/n" << endl;
 		cin >> option;
 		if (option == 'n')
